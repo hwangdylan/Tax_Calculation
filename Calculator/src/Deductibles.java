260ac151ac7fb2e60681681ec63974208e972582
Dylan Hwang <dylanhwang@berkeley.edu>
2018-09-12 15:06:18 -0700

    added stuff

--- Calculator/src/Deductibles.java
+++ Calculator/src/Deductibles.java
@@ -1,4 +1,9 @@
 
 /** calculates the deductibles*/
 public class Deductibles {
+    private Double totalDeductions;
+
+    public Double getTotalDeductions() {
+        return totalDeductions;
+    }
 }
