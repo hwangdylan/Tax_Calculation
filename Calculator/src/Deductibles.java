52c650151bd65a675717fa516ba6b254c5e0d160
doused <pucker.up.and@gmail.com>
2018-09-12 16:39:15 -0700

    starting to outline deduction types

--- Calculator/src/Deductibles.java
+++ Calculator/src/Deductibles.java
@@ -1,9 +1,33 @@
+import java.util.Scanner; //actually we'll take care of user input later
 
 /** calculates the deductibles*/
 public class Deductibles {
     private Double totalDeductions;
 
+
+    public Double calculateDependents() { //calculate deductions due to dependents
+
+    }
+
+    //also have a function for retirement plan/IRA deductions, since those are also not itemized
+
+    public Double calculateItemizedDeductions(Double AGI, Double MAGI) {
+        Double rtn = 0;
+
+        //From charitable donations:
+        //percentage limit for charitable donations is 60% of user's AGI
+
+
+        //From medical/dental expenses:
+        //calculate the "floor" amount based on AGI; we can only deduct amounts that exceed the floor
+
+        //From state and local taxes:
+    }
     public Double getTotalDeductions() {
+        //here, determine whether itemized deductions exceed standard deduction based on filing status
+        /*possibly: have separate classes for 1. itemized deductions and 2. all other deductions?
+        Since the itemized deductions are compared to the standard deduction.
+        But that's an organization issue that we can deal with later whatever */
         return totalDeductions;
     }
 }
