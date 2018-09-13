e27811baa02ed2ccafebdd4a263fe92b27d500c4
doused <pucker.up.and@gmail.com>
2018-09-12 22:22:15 -0700

    adding some constants

--- Calculator/src/FilingStatus.java
+++ Calculator/src/FilingStatus.java
@@ -6,4 +6,8 @@ public class FilingStatus {
      * 2: Married filing jointly
      * 3: Married filing seperately
      * 4: Head of House-hold*/
+    public static final int SINGLE=1;
+    public static final int MARRIED_JOINT=2;
+    public static final int MARRIED_SEPARATE=3;
+    public static final int HEAD_OF_HOUSEHOLD=4;
 }
