1003f7224fba2a77f5966cfdd5c4b6ea1275e152
Dylan Hwang <dylanhwang@berkeley.edu>
2018-10-10 14:31:00 -0700

    add magi class

--- Calculator/src/AdjustedGrossIncome.java
+++ Calculator/src/AdjustedGrossIncome.java
@@ -1,5 +1,7 @@
 public class AdjustedGrossIncome {
 
+
+    private long totalDeductions;
     private long adjustedGrossIncome;
     private long iRAContributions;
     private long studentLoanInterest;
@@ -17,34 +19,40 @@ public class AdjustedGrossIncome {
      */
     public AdjustedGrossIncome(long grossIncome) {
         this.adjustedGrossIncome = grossIncome;
+        totalDeductions = 0;
     }
+
+
     /**sets each income type and then adds to gross income*/
     public void setiRAContributions(long iRAContributions) {
         this.iRAContributions = iRAContributions;
         adjustedGrossIncome -= iRAContributions;
+        totalDeductions += iRAContributions;
     }
     public void setStudentLoanInterest(long studentLoanInterest) {
         this.studentLoanInterest = studentLoanInterest;
         adjustedGrossIncome -= iRAContributions;
+        totalDeductions += studentLoanInterest;
     }
     public void setMovingExpenses(long movingExpenses) {
         this.movingExpenses = movingExpenses;
         adjustedGrossIncome -= iRAContributions;
+        totalDeductions += movingExpenses;
     }
     public void setHalfSelfEmployment(long halfSelfEmployment) {
         this.halfSelfEmployment = halfSelfEmployment;
         adjustedGrossIncome -= iRAContributions;
+        totalDeductions += halfSelfEmployment;
     }
     public void setsEPContributions(long sEPContributions) {
         this.sEPContributions = sEPContributions;
         adjustedGrossIncome -= iRAContributions;
+        totalDeductions += sEPContributions;
     }
-
-
     public double getAdjustedGrossIncome() {
         return adjustedGrossIncome;
     }
-    public void setAdjustedGrossIncome() {
+    public void setAdjustedGrossIncome(long adjustedGrossIncome) {
         //here, above-the-line deductions ("adjustments") are taken to arrive at user's AGI
         // https://www.irs.gov/pub/irs-pdf/f1040.pdf all adjustments are here
 
@@ -57,8 +65,8 @@ public class AdjustedGrossIncome {
 
         //taxable income = adjusted gross income - exemptions - itemized deductions (or standard deduction)
         //no such thing as personal exemptions for 2018, standard deduction is just higher
+        this.adjustedGrossIncome = adjustedGrossIncome;
 
-        
     }
 
 }
---
+++ Calculator/src/ModifiedAdjustedGrossIncome.java
@@ -0,0 +1,11 @@
+public class ModifiedAdjustedGrossIncome {
+
+
+    private long modifiedAGI;
+
+
+
+    public ModifiedAdjustedGrossIncome(long adjustedGrossIncome) {
+        modifiedAGI = adjustedGrossIncome;
+    }
+}
